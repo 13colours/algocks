4baeaff08cc52b5a4c382e60ee348dab8e782b03
13Colours <tasker.prabhat@gmail.com>
2021-06-15 00:18:55 +1000

    Add logic for Object endpoints

--- src/routes/object.ts
+++ src/routes/object.ts
@@ -1,4 +1,8 @@
 import express from 'express'
+import { v4 as uuidv4 } from 'uuid'
+
+import { indexer } from '..'
+
 const router = express.Router()
 
 /**
@@ -8,12 +12,67 @@ const router = express.Router()
  * https://www.algolia.com/doc/rest-api/search/#batch-write-operations-multiple-indices
  */
 router.post(`/:indexName/batch`, function (req, res) {
-  const batchOperation = req.params.indexName === `*`
-  res.json({
-    message: `Batch write operations ${
-      batchOperation ? `(multiple indices)` : ``
-    }`,
-  })
+  const {
+    params,
+    body: { requests },
+  } = req
+
+  try {
+    const objectIDs = []
+
+    requests.forEach((request) => {
+      const { action, indexName: requesIndexName, body } = request
+
+      const indexName = requesIndexName || params.indexName
+      const index = indexer.getIndex(indexName)
+
+      const tempID = uuidv4()
+
+      switch (action) {
+        case `addObject`:
+          index.add({ objectID: body.objectID || tempID, ...body })
+          objectIDs.push(tempID)
+          break
+
+        case `updateObject`:
+          if (!body.objectID) throw new Error(`Missing objectID`)
+
+          index.add(body)
+          objectIDs.push(body.objectID)
+          break
+
+        case `partialUpdateObject`:
+          if (!body.objectID) throw new Error(`Missing objectID`)
+
+          index.update({ ...index.get(body.objectID), ...body })
+          objectIDs.push(body.objectID)
+          break
+
+        case `partialUpdateObjectNoCreate`:
+          if (!body.objectID) throw new Error(`Missing objectID`)
+          if (!indexer.objectIDExists(indexName, body.objectID)) {
+            throw new Error(`objectID not found`)
+          }
+
+          index.update({ ...index.get(body.objectID), ...body })
+          objectIDs.push(body.objectID)
+          break
+
+        case `deleteObject`:
+          if (!body.objectID) throw new Error(`Missing objectID`)
+
+          index.remove(body.objectID)
+          break
+
+        default:
+          throw new Error(`Unknown request type`)
+      }
+    })
+
+    res.json({ objectIDs })
+  } catch (e) {
+    res.status(400).json({ message: e.message })
+  }
 })
 
 /**
@@ -21,7 +80,14 @@ router.post(`/:indexName/batch`, function (req, res) {
  * https://www.algolia.com/doc/rest-api/search/#clear-objects
  */
 router.post(`/:indexName/clear`, function (req, res) {
-  res.json({ message: `Clear objects` })
+  const {
+    params: { indexName },
+  } = req
+
+  const index = indexer.getIndex(indexName)
+  index.store = {}
+
+  res.json({ updatedAt: new Date(Date.now()) })
 })
 
 /**
@@ -29,7 +95,22 @@ router.post(`/:indexName/clear`, function (req, res) {
  * https://www.algolia.com/doc/rest-api/search/#get-object
  */
 router.get(`/:indexName/:objectID`, function (req, res) {
-  res.json({ message: `Get object` })
+  const {
+    params: { indexName, objectID },
+  } = req
+
+  try {
+    if (!indexer.objectIDExists(indexName, objectID)) {
+      throw new Error(`objectID not found`)
+    }
+
+    const index = indexer.getIndex(indexName)
+    const result = index.get(objectID)
+
+    res.json(result)
+  } catch (e) {
+    res.status(400).json({ message: e.message })
+  }
 })
 
 /**
@@ -37,7 +118,29 @@ router.get(`/:indexName/:objectID`, function (req, res) {
  * https://www.algolia.com/doc/rest-api/search/#get-objects
  */
 router.post(`/*/objects`, function (req, res) {
-  res.json({ message: `Get objects` })
+  const {
+    body: { requests },
+  } = req
+
+  try {
+    const results = []
+
+    requests.forEach((request) => {
+      const { indexName, objectID } = request
+
+      if (!indexer.objectIDExists(indexName, objectID)) {
+        throw new Error(`objectID not found`)
+      }
+
+      const index = indexer.getIndex(indexName)
+      const document = index.get(objectID)
+      results.push(document)
+    })
+
+    res.json({ results })
+  } catch (e) {
+    res.status(400).json({ message: e.message })
+  }
 })
 
 export default router
